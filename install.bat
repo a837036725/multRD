5cae344da82b0a5d3a835c7337a37e151fad6308
a837036725 <51903414+a837036725@users.noreply.github.com>
2021-05-21 12:03:53 +0800

    Update install.bat
    Auto find the ReleaseId.

--- install.bat
+++ install.bat
@@ -3,61 +3,18 @@
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
 
-winver
+for /f "tokens=3"  %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "ReleaseId"') do (
+set num=%%i
+)
 
-:chover
-echo.
-echo  ===============================
-echo         请选择系统版本
-echo  ===============================
-echo.
-echo  1. 1809
-echo.
-echo  2. 1903
-echo.
-echo  3. 1909
-echo.
-echo  4. 2004
-echo.
-echo  5. 202H
-echo.
-echo  6. server2019
-echo.
-echo  7. 手动输入
-echo.
-
-set rtype=
-set /p rtype=  please input the number:
-IF NOT "%rtype%"=="" SET rtype=%rtype:~0,1%
-if /i "%rtype%"=="1" (
-set num=1809
+if exist .\termsrv\termsrv.dll.%num% (
 goto main
+) else (
+echo Can not find the right dll file in termsrv folder, please put it into the termsrv folder and try again
+echo File type: termsrv.dll.ReleaseId
+echo Your ReleaseId: %num%
+goto end
 )
-if /i "%rtype%"=="2" (
-set num=1903
-goto main
-)
-if /i "%rtype%"=="3" (
-set num=1909
-goto main
-)
-if /i "%rtype%"=="4" (
-set num=2004
-goto main
-)
-if /i "%rtype%"=="5" (
-set num=202H
-goto main
-)
-if /i "%rtype%"=="6" (
-set num=server2019
-goto main
-)
-if /i "%rtype%"=="7" (
-set /p num= 请输入版本号:
-goto main
-)
-goto chover
 
 :main
 net stop TermService
@@ -70,18 +27,13 @@ del .\chtm
 if exist %bakpath% (
 move %bakpath% "C:\Windows\System32\termsrv.dll_%dt%.bak"
 move C:\Windows\System32\termsrv.dll %bakpath%
-) else if exist .\termsrv\termsrv.dll.%num% (
-move C:\Windows\System32\termsrv.dll %bakpath%
 ) else (
-echo 未在termsrv文件夹中找到相应的dll文件，请放入文件后重试
-echo 文件格式为：termsrv.dll.版本号
-goto end
+move C:\Windows\System32\termsrv.dll %bakpath%
 )
 
 copy .\termsrv\termsrv.dll.%num% C:\Windows\System32\termsrv.dll
-
-:end
 net start TermService
 
+:end
 pause
 @echo on
