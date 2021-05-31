a96ff672e6a0968d610e6c019573d4c2e7b9c53c
a837036725 <51903414+a837036725@users.noreply.github.com>
2021-05-31 15:09:20 +0800

    Update uninstall.bat

--- uninstall.bat
+++ uninstall.bat
@@ -3,7 +3,7 @@
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
 
-net stop termservice
+net stop termservice /y
 
 takeown /f C:\Windows\System32\termsrv.dll
 cacls C:\Windows\System32\termsrv.dll /E /C /G everyone:F
@@ -11,7 +11,7 @@ cacls C:\Windows\System32\termsrv.dll /E /C /G everyone:F
 del C:\Windows\System32\termsrv.dll
 if exist C:\Windows\System32\termsrv.dll.bak (
 move C:\Windows\System32\termsrv.dll.bak C:\Windows\System32\termsrv.dll
-net start termservice
+net start termservice /y
 ) else (
 echo Backup file loss, please manully copy termsrv.dll to C:\Windows\System32\
 )
